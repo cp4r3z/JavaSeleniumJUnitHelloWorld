6cdf1b643c6479a07e359e2c55a6db7a23a47985
cp4r3z <cp4r3z@gmail.com>
2019-08-24 20:30:39 -0500

    Adding chromedriver
    Probably bad practice to commit chromedriver binary

---
+++ src/main/java/info/parez/Main.java
@@ -0,0 +1,29 @@
+package info.parez;
+
+//import java.lang.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String test = "Hello ChromeDriver";
+        System.out.println(test);
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+//        driver.close();
+        driver.quit();
+
+        System.exit(0);
+    }
+}
